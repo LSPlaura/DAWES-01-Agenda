4468964cd3ef75c3190db86c0f706ae7f536bb24
Laura <lsp3114@gmail.com>
2026-09-13 19:12:23 +0200

    feat(config): add CacheCapacity setting

--- AgendaContactos.Back/Configuration/Config.cs
+++ AgendaContactos.Back/Configuration/Config.cs
@@ -4,4 +4,5 @@ public static class Config
 {
     public static readonly string DefaultCountryCode = "+34";
     public static readonly string DataBaseFolder = "ContactData";
+    public static readonly int CacheCapacity = 5;
 }
