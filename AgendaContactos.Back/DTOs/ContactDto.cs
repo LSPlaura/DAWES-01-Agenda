daf770e7004da9224860ca69c79eb435b1d3e199
Laura <lsp3114@gmail.com>
2026-09-13 14:28:08 +0200

    feat(dto): allow nullable properties in ContactDto

--- AgendaContactos.Back/DTOs/ContactDto.cs
+++ AgendaContactos.Back/DTOs/ContactDto.cs
@@ -4,8 +4,8 @@ namespace AgendaContactos.Back.DTOs;
 /// Objeto de transferencia de datos para la creación de un nuevo contacto.
 /// </summary>
 public sealed record ContactDto(
-    string PhoneNumber,
-    string Name,
+    string? PhoneNumber,
+    string? Name,
     string? Alias,
-    string Email
+    string? Email
 );
