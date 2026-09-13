b7d46efae8caf2673c4c3a1a5629b2a1219936e0
Laura <lsp3114@gmail.com>
2026-09-13 21:15:50 +0200

    feat(services): add IcontactService interface

---
+++ AgendaContactos.Back/Services/Crud/Contacts/IContactsService.cs
@@ -0,0 +1,17 @@
+using AgendaContactos.Back.Models;
+using AgendaContactos.Back.Services.Crud.Common;
+
+namespace AgendaContactos.Back.Services.Crud.Contacts;
+
+/// <summary>
+/// Interfaz para el servicio de gestión de contactos con lógica de negocio.
+/// </summary>
+public interface IContactsService : ICrudService<string, Contact>
+{
+    /// <summary>
+    /// Obtiene una colección de contactos filtrados por su alias.
+    /// </summary>
+    /// <param name="alias">Alias por el cual filtrar.</param>
+    /// <returns>Una colección de contactos coincidentes.</returns>
+    IEnumerable<Contact> GetByAlias(string alias);
+}
