5a01e0d9bed4c1df13241245903378b65a0633c3
Laura <lsp3114@gmail.com>
2026-09-13 15:20:29 +0200

    refactor(validators): Change function name in IValidate fron Validar to Validate

--- AgendaContactos.Back/Validators/Common/IValidate.cs
+++ AgendaContactos.Back/Validators/Common/IValidate.cs
@@ -1,8 +1,7 @@
-using AgendaContactos.Back.Errors;
 using AgendaContactos.Back.Errors.Common;
 using CSharpFunctionalExtensions;
 
-namespace AgendaContactos.Back.Validators;
+namespace AgendaContactos.Back.Validators.Common;
 
 /// <summary>
 /// Interfaz genérica para la inversión de dependencias en la validación de dominio.
@@ -12,12 +11,12 @@ public interface IValidate<T>
 {
     /// <summary>
     /// Ejecuta las reglas de validación requeridas y devuelve un <see cref="Result{T, E}"/> 
-    /// que contiene <c>true</c> en caso de éxito o un <see cref="DomainError"/> en caso de fallo.
+    /// que contiene <c>true</c> en caso de éxito o un <see cref="item"/> en caso de fallo.
     /// </summary>
     /// <param name="item">La instancia a validar.</param>
     /// <returns>
-    /// Un <see cref="Result{T, E}"/> que indica éxito (<c>true</c>) 
+    /// Un <see cref="DomainError"/> que indica éxito (<c>true</c>) 
     /// o fallo con el correspondiente <see cref="DomainError"/>.
     /// </returns>
-    Result<bool, DomainError> Validar(T item);
+    Result<bool, DomainError> Validate(T item);
 }
