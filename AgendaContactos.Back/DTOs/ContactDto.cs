88b42705b2943646723880952b8744117b31b138
Laura <lsp3114@gmail.com>
2026-09-13 13:48:07 +0200

    refactor(domain): rename CreateContactDto to ContactDto

--- AgendaContactos.Back/DTOs/ContactDto.cs
+++ AgendaContactos.Back/DTOs/ContactDto.cs
@@ -3,7 +3,7 @@ namespace AgendaContactos.Back.DTOs;
 /// <summary>
 /// Objeto de transferencia de datos para la creación de un nuevo contacto.
 /// </summary>
-public sealed record CreateContactDto(
+public sealed record ContactDto(
     string PhoneNumber,
     string Name,
     string? Alias,
