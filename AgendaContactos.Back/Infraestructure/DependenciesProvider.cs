766b3479c95c970772f92bc651e29caa2a168e8b
Laura <lsp3114@gmail.com>
2026-09-13 23:48:08 +0200

    feat(infraestructure): add dependencies provider

---
+++ AgendaContactos.Back/Infraestructure/DependenciesProvider.cs
@@ -0,0 +1,50 @@
+using AgendaContactos.Back.Cache;
+using AgendaContactos.Back.Cache.Common;
+using AgendaContactos.Back.Models;
+using AgendaContactos.Back.Repositories;
+using AgendaContactos.Back.Repositories.Contacts;
+using AgendaContactos.Back.Services.Crud.Contacts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgendaContactos.Back.Infraestructure;
+
+public static class DependenciesProvider
+{
+    public static IServiceProvider BuildServiceProvider()
+    {
+        var services = new ServiceCollection();
+        
+        RegisterRepository(services);
+        RegisterCache(services);
+        RegisterServices(services);
+        
+        return services.BuildServiceProvider();
+    }
+
+    private static void RegisterRepository(IServiceCollection services)
+    {
+        services.AddSingleton<AppDbContext>(sp => 
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite("Data Source=agenda.db");
+        
+            return new AppDbContext(optionsBuilder.Options);
+        });
+
+        services.AddSingleton<IContactRepository, ContactRepository>();
+    }
+    
+    private static void RegisterCache(IServiceCollection services)
+    {
+        services.AddSingleton<ICache<string, Contact>, LruCache>();
+    }
+    
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.AddTransient<IContactsService, ContactService>(sp => new ContactService(
+            sp.GetRequiredService<IContactRepository>(),
+            sp.GetRequiredService<ICache<string, Contact>>()
+        ));
+    }
+}
